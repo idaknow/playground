803b411eccd9e543fc5e4e0d01862096dc6f9805
Ida <ides542@aucklanduni.ac.nz>
2023-12-27 11:35:46 -0800

    Update to ts, add routing and simple home page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+
+import Home from "./routes/Home";
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+export default App;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <p>
+      Home is where the heart is x
+    </p>
+  );
+}
+
+export default App;
