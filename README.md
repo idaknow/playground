9a5af16a3b6f00d4d1edafddf91600ec76fcc865
Ida <ides542@aucklanduni.ac.nz>
2024-01-07 11:40:07 -0800

    add stackblitz

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 > [!CAUTION]
 > This is under construction, view at your own risk, not guaranteed to be stable
+> Additionally note that StackBlitz does **not** support csv / png files in my repo, so the dev server errors out
 
 Welcome to Ida's web development playground. No real purpose except learning, creativity and fun.
-
-Hire me @ https://www.linkedin.com/in/idakdesmet/
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -8,48 +8,75 @@ import { skillIcons, skills } from "../cvs/Skills";
 
 import logo from "../img/logo.png";
 
+const stackBlitzUrl =
+  "https://stackblitz.com/github/idaknow/playground?title=Idas playground&view=editor";
+
 const Home = () => {
   return (
-    <Grid
-      container
-      spacing={2}
-      style={{ backgroundColor: theme.palette.primary.dark, height: "auto" }}
+    <div
+      style={{
+        backgroundColor: theme.palette.primary.dark,
+        height: "auto",
+      }}
     >
-      <Grid item xs={6} key="logo" id="logo">
-        <img
-          id="ida-logo"
-          key="ida-logo"
-          src={logo}
-          alt="my cool logo"
-          style={{ opacity: 0.5, width: "100%" }}
-        />
+      <Grid container spacing={2}>
+        <Grid item xs={6} key="logo" id="logo">
+          <img
+            id="ida-logo"
+            key="ida-logo"
+            src={logo}
+            alt="my cool logo"
+            style={{ opacity: 0.5, width: "100%" }}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          style={{ color: theme.palette.primary.light, marginBottom: "20px" }}
+          key="ida-cv"
+          id="ida-cv"
+        >
+          <h1>Skills</h1>
+          {skills.map(({ icon, text, id }) => {
+            const Icon = skillIcons[icon];
+            return (
+              <InlineIconText text={text} key={id}>
+                <Icon />
+              </InlineIconText>
+            );
+          })}
+
+          <h1>Experience</h1>
+          {experiences.map((experience) => (
+            <ExperienceAccordion
+              experience={experience}
+              key={experience.company.replace(" ", "-")}
+            />
+          ))}
+        </Grid>
       </Grid>
       <Grid
-        item
-        xs={6}
-        style={{ color: theme.palette.primary.light, marginBottom: "20px" }}
-        key="ida-cv"
-        id="ida-cv"
+        container
+        spacing={1}
+        style={{
+          color: theme.palette.primary.light,
+        }}
       >
-        <h1>Skills</h1>
-        {skills.map(({ icon, text, id }) => {
-          const Icon = skillIcons[icon];
-          return (
-            <InlineIconText text={text} key={id}>
-              <Icon />
-            </InlineIconText>
-          );
-        })}
-
-        <h1>Experience</h1>
-        {experiences.map((experience) => (
-          <ExperienceAccordion
-            experience={experience}
-            key={experience.company.replace(" ", "-")}
-          />
-        ))}
+        <Grid item xs={12} style={{ margin: "80px" }}>
+          <h1>✨View the code powering this website✨</h1>
+          <iframe
+            src={stackBlitzUrl}
+            title="StackBlitz this website"
+            style={{
+              height: "auto",
+              width: "95%",
+              minHeight: "500px",
+              marginBottom: "100px",
+            }}
+          ></iframe>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 };
 
