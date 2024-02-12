048e537acf584cd4189b7e6c2f7b3b64a1486b53
Ida <ides542@aucklanduni.ac.nz>
2024-02-11 16:49:17 -0800

    bottom navigation

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,15 @@
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+
 import CodeSource from "../components/CodeSource";
 import Experience from "../components/Experience";
 import MainIntroduction from "../components/MainIntroduction";
 import Proficiency from "../components/Proficiency";
+import { palette } from "../components/palette";
+import Logo from "../img/logo-without-text.png";
 
 const Home = () => {
   return (
@@ -10,6 +18,44 @@ const Home = () => {
       <Experience />
       <Proficiency />
       <CodeSource />
+      <BottomNavigation
+        showLabels
+        sx={{
+          backgroundColor: palette.dark,
+          paddingBottom: "50px",
+        }}
+      >
+        <BottomNavigationAction
+          label="Ida De Smet"
+          icon={
+            <img
+              src={Logo}
+              alt=""
+              style={{ height: "1.4rem", width: "1.4rem" }}
+            />
+          }
+          sx={{ color: palette.secondary }}
+          href="#"
+        />
+        <BottomNavigationAction
+          label="LinkedIn"
+          icon={<LinkedInIcon />}
+          sx={{ color: palette.secondary }}
+          href="https://www.linkedin.com/in/idakdesmet/"
+        />
+        <BottomNavigationAction
+          label="GitHub"
+          icon={<GitHubIcon />}
+          sx={{ color: palette.secondary }}
+          href="https://github.com/idaknow"
+        />
+        <BottomNavigationAction
+          label="North Vancouver"
+          icon={<LocationOnIcon />}
+          sx={{ color: palette.secondary }}
+          disabled
+        />
+      </BottomNavigation>
     </>
   );
 };
